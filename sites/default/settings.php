7a8a62fc10f4e9d7ec155a54ffba2cac5410120e
codefreezing <codefreezing@gmail.com>
2011-10-06 11:15:52 +0700

    05/10/2012

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -88,7 +88,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-$db_url = 'mysql://username:password@localhost/databasename';
+$db_url = 'mysql://root:@localhost/zooday';
 $db_prefix = '';
 
 /**
